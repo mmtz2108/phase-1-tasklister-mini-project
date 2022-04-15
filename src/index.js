7721e931a6720c81b32ba23b345f3b7b18d332e6
Marcos Martinez <mmtz2108@gmail.com>
2022-04-14 22:20:14 -0500

    rendered a task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  buildToDo()
+
 });
+ const form = document.querySelector('#create-task-form')
+ form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDo(e.target['new-task-description'].value)
+  form.reset();
+ })
+
+ const buildToDo = (todo) => {
+   let p  = document.createElement('p')
+   let btn = document.createElement('button')
+   btn.addEventListener('click', handleDelete)
+  
+   p.textContent = `${todo}`
+   btn.textContent = 'x'
+   p.appendChild(btn)
+   document.querySelector('#tasks').append(p)
+
+ }
+
+const handleDelete = (e) => {
+  e.target.parentNode.remove()
+}
+
